8f4dc1912d4d3a0b3c45441a7206ee10c63b8c72
Fabian295 <fabiankanhai70@gmail.com>
2022-09-25 04:12:48 +0200

    committed

--- start/src/App.js
+++ start/src/App.js
@@ -1,9 +1,25 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1)
+  } 
+
   return (
-    <div>App, Hello World!</div>
+    <div>
+      <h1>Working with React</h1>
+      App, Hello World!
+    
+      <p>{counter}</p>
+      <p>
+        <button onClick={increment}>Increment</button>
+      </p>
+    
+    </div>
   )
 }
 
